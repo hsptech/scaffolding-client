6ed1c750368281f57b5dad52c69bf66b194ba291
hsptech <hsptech.bandi@gmail.com>
2023-10-19 07:50:10 -0500

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "../Header";
+import LandingPage from "../LandingPage";
+import Footer from "../Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <LandingPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
